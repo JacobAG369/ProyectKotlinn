0e508d4eeb955694222bf5cacb91561ce315fe54
JacobAG369 <jmy.369@outlook.com>
2026-06-05 01:57:54 -0600

    Ls1

---
+++ smartt/src/main/java/com/example/miprimerrepositorio/presentation/Clase2.kt
@@ -0,0 +1,13 @@
+package com.example.miprimerrepositorio.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.miprimerrepositorio.R
+
+class Clase2 : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.prueba)
+    }
+}
